608915ebce8cbdd58d72a4bda0d7a437ee1994e7
Cesar Martinez <cesar.martinez.perez.415@gmail.com>
2021-03-14 15:04:10 -0700

    Added questionsto ask the user about the structure they want for their read me file. Added some authentication for some questions. Using inquirer to promt the user and collect their answers. created function to write to file send the data collected and see if there is any kind of error.

--- index.js
+++ index.js
@@ -1,13 +1,163 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const choice = require('inquerer/lib;objects/choice');
+const choices = require('inquirer/lib/objects choices');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = () => {
+return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter name of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the link to your Github profile?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github link!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project!',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide instructions for installation!',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide installation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide how to use it!',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions on how to use it!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which licenses apply',
+        choices: [
+            {
+                name: 'MIT',
+                value: {
+                    text: 'MIT',
+                    url: "https://choosealicense.com/licenses/mit/",
+                    badge: "![NPM](https://img.shields.io/npm/l/license)"
+                }
+            },
+            {
+                name: 'GNU AGPLv3',
+                value: {
+                    text: 'GNU AGPLv3',
+                    url: "https://choosealicense.com/licenses/agpl-3.0/",
+                    badge: "![NPM](https://img.shields.io/badge/GNU_AGPv3-License-orange)"
+                }
+            },
+            {
+                name: 'Mozilla',
+                value: {
+                    text: 'Mozilla',
+                    url: "https://choosealicense.com/licenses/mpl-2.0/",
+                    badge: "![NPM](https://img.shields.io/badge/Mozilla-License-orange)"
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+                }
+            },
+            {
+                name: 'The Unlicense',
+                value: {
+                    text: 'The Unlicense',
+                    url: "https://choosealicense.com/licenses/unlicense/",
+                    badge: "![NPM](https://img.shields.io/badge/Unlicense-License-orange)"
+                }
+            }
+        ]
+    }
+])
+};
+// Create a function to write README file
+const writeToFile = (fileName, data) => { 
+    
+    return new Promise((resolve, reject) => {
+        fs.writeFile ('./README.md', fileName, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
 
-// TODO: Create a function to initialize app
-function init() {}
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: "Your file is created!"
+            });
+        });
+    });
+};
+
+// Create a function to initialize app
+function init() {
+    questions()
+        .then(generateMarkdown)
+        .then(writeToFile)
+        .catch(err => {
+            console.log(err);
+        });
+ }
 
 // Function call to initialize app
 init();
